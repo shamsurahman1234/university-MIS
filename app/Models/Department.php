6a960a3ca30af7814d6af9de15da3adb1593055a
Shamsu Rahman <your-email@example.com>
2025-09-21 22:28:17 +0430

    change the department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,19 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'faculty_id'];
 
     public function faculty()
     {
         return $this->belongsTo(Faculty::class);
-    }
-
-    public function university()
-    {
-        return $this->faculty->university();
     }
 }
